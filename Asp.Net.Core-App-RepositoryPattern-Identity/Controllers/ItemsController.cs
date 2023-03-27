aee67867447306f617d159d3902056e00f4cb973
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-03-27 11:28:11 +0100

    Create item Controller

---
+++ Asp.Net.Core-App-RepositoryPattern-Identity/Controllers/ItemsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net.Core_App_RepositoryPattern_Identity.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
