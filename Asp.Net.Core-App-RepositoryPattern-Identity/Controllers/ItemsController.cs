0deaf679cbabfcfa8620695ed50a34dc0a4466ba
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-03-27 11:55:11 +0100

    Add Items List Screen

--- Asp.Net.Core-App-RepositoryPattern-Identity/Controllers/ItemsController.cs
+++ Asp.Net.Core-App-RepositoryPattern-Identity/Controllers/ItemsController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.Net.Core_App_RepositoryPattern_Identity.Data;
+using Asp.Net.Core_App_RepositoryPattern_Identity.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.Net.Core_App_RepositoryPattern_Identity.Controllers
 {
     public class ItemsController : Controller
     {
+        public ItemsController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        private readonly AppDbContext _db;
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Item> itemsList = _db.Items.ToList();
+            return View(itemsList);
         }
     }
 }
