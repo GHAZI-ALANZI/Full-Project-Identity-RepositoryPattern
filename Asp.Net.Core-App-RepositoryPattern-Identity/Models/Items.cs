f77f22ef232901ba3311b90f592511c8304739be
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-03-27 11:10:11 +0100

    Add project  files

---
+++ Asp.Net.Core-App-RepositoryPattern-Identity/Models/Items.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asp.Net.Core_App_RepositoryPattern_Identity.Models
+{
+    public class Items
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
