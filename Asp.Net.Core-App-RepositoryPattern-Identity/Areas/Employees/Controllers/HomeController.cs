5087ab5160da0e3a9cbdd2f0cd4b50d4c648236e
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-04-05 15:18:01 +0100

    Using Areas in MVC .Net Core

---
+++ Asp.Net.Core-App-RepositoryPattern-Identity/Areas/Employees/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net.Core_App_RepositoryPattern_Identity.Areas.Employees.Controllers
+{
+
+    [Area("Employees")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Asp.Net.Core-App-RepositoryPattern-Identity/Program.cs
+++ Asp.Net.Core-App-RepositoryPattern-Identity/Program.cs
@@ -24,6 +24,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.MapControllerRoute(
+    name: "area",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
